35ac0ae9de16f4e6851d45739369c5ac6fdf3ee3
feoalexei <feoalexei@ukr.net>
2020-06-28 21:29:09 +0300

    solved

--- Running Sum of 1d Array/index.js
+++ Running Sum of 1d Array/index.js
@@ -1,8 +1,11 @@
 ////////// 1480. Running Sum of 1d Array //////////
 
 var runningSum = function(nums) {
-   var output = [];
-   nums.map((elem, index) => {
-       
-   });
+    var output = [nums[0]];
+    for (let i = 1; i < nums.length; i++) {
+        output.push(nums[i] + output[i - 1]);
+   }
+   return output;
 };
+
+console.log(runningSum([1,2,3,4]));
