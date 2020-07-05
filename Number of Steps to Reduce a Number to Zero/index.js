400d2b79675579e2f920dd827018ab0ba4b1fad5
feoalexei <feoalexei@ukr.net>
2020-07-05 16:52:48 +0300

    easy

---
+++ Number of Steps to Reduce a Number to Zero/index.js
@@ -0,0 +1,19 @@
+////////// 1342. Number of Steps to Reduce a Number to Zero //////////
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const numberOfSteps  = function(num) {
+    let counter = 0;
+    while (num) {
+        num % 2 ? num-- 
+                : num /= 2; 
+                 
+        counter++;
+    }
+    return counter;
+};
+
+console.log(numberOfSteps(8));
+
